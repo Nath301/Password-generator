84bb1aa4269b9429256853c19f0ce56910f54d8a
Glaive <nathan.nancy@icloud.com>
2020-06-06 13:22:07 -0400

    code shortening

--- password_generator.py
+++ password_generator.py
@@ -2,10 +2,10 @@ import random
 lst = []
 def random_letter():
 
-  while len(lst) < 20:
+  while len(lst) < 12:
     letter = random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890&_-@$./')
     lst.append(letter)
     password = "".join(lst)
   return password
 
-print('Your new password is : ' + random_letter())
+print('Your new password is : {}'.format(random_letter()))
